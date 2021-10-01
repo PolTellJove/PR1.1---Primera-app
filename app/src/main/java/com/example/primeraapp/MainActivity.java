bdf9d54703597ee102c9dc6402cc65b6317bd553
PolTellJove <poltelljove@gmail.com>
2021-10-01 19:09:16 +0200

    Primera app

---
+++ app/src/main/java/com/example/primeraapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.primeraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Toast.makeText(MainActivity.this, "Button pressed", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
